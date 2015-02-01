cabad3d431848ee7bf4db200da31f9694f3d0dc8
2015lucyb <bermanlucy@yahoo.com>
2015-02-01 09:35:30 -0500

    Initial Commit

---
+++ Attendance2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Attendance2
+//
+//  Created by Lucy Berman on 2/1/15.
+//  Copyright (c) 2015 Lucy Berman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
