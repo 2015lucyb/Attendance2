06ab2254c3049bf6cdccbf7888b1e450e08e2dbf
2015lucyb <bermanlucy@yahoo.com>
2015-02-01 09:47:27 -0500

    2 text fields and button

---
+++ Attendance2/StudentViewController.h
@@ -0,0 +1,13 @@
+//
+//  StudentViewController.h
+//  Attendance2
+//
+//  Created by Lucy Berman on 2/1/15.
+//  Copyright (c) 2015 Lucy Berman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StudentViewController : UIViewController
+
+@end
---
+++ Attendance2/StudentViewController.m
@@ -0,0 +1,39 @@
+//
+//  StudentViewController.m
+//  Attendance2
+//
+//  Created by Lucy Berman on 2/1/15.
+//  Copyright (c) 2015 Lucy Berman. All rights reserved.
+//
+
+#import "StudentViewController.h"
+
+@interface StudentViewController ()
+
+@end
+
+@implementation StudentViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSLog(@"test");
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
