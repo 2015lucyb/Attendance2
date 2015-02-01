94e97984b7733adb3f5897206093baa34dffc9b4
eyw410 <eyw410@gmail.com>
2015-02-01 09:59:50 -0500

    added faculty/student buttons

---
+++ Attendance2/FacultyViewController.h
@@ -0,0 +1,13 @@
+//
+//  CasadoViewController.h
+//  Attendance2
+//
+//  Created by Developer on 2/1/15.
+//  Copyright (c) 2015 Lucy Berman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FacultyViewController : UIViewController
+
+@end
---
+++ Attendance2/FacultyViewController.m
@@ -0,0 +1,37 @@
+//
+//  CasadoViewController.m
+//  Attendance2
+//
+//  Created by Developer on 2/1/15.
+//  Copyright (c) 2015 Lucy Berman. All rights reserved.
+//
+
+#import "FacultyViewController.h"
+
+@interface FacultyViewController ()
+
+@end
+
+@implementation FacultyViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
